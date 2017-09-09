8f1e47eb9eb85d9d35da99f49c7bf0befa53357f
Aaron Cordova <theqodesmith@gmail.com>
2017-09-09 21:20:15 +0200

    Added option for the "onprogress" event.

--- BinaryTransport/jquery.binarytransport.js
+++ BinaryTransport/jquery.binarytransport.js
@@ -29,6 +29,10 @@
                         username = options.username || null,
                         password = options.password || null;
 
+                    if (options.onprogress) {
+                        xhr.onprogress = options.onprogress;
+                    }
+
                     this.xhr = xhr;
 
                     xhr.addEventListener('load', function() {
